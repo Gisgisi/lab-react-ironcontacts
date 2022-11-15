b1ec4ffd3b2c99bb78a1bf8c8ff439656ab7b644
John Doe <atarod.shirin@gmail.com>
2022-11-15 22:01:45 +0100

    iteration3

--- src/App.js
+++ src/App.js
@@ -5,14 +5,28 @@ import { startTransition, useState } from 'react';
 
 
 function App() {
-  const [contacts, setContacts] = useState(ContactsJSON.slice(0, 5))
-  console.log(contacts)
+  const [contacts, setContacts] = useState(ContactsJSON.slice(0, 5));
+  
+
+  let getRandomInt = function(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  let randomStar = function() {
+    let randomContactIndex = getRandomInt(5, ContactsJSON.length - 1);
+    let randomContact = ContactsJSON[randomContactIndex];
+    
+    setContacts([...contacts, randomContact]);
+    ContactsJSON.splice(randomContactIndex, 1);
+  }
   return (
     <div className="App">
       <h1>IronContacts</h1>
       <table>
         <thead>
-          <tr class='tablehead'>
+          <tr className='tablehead'>
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
@@ -21,19 +35,18 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          <tr>
             {contacts.map((star) =>
               <tr key={star.id}>
-                <td><img class='starimg' src={star.pictureUrl}/></td>
+                <td><img className='starimg' src={star.pictureUrl} /></td>
                 <td>{star.name}</td>
-                <td>{(Math.round((star.popularity)*100))/100}</td>
+                <td>{(Math.round((star.popularity) * 100)) / 100}</td>
                 {(star.wonOscar === true) && <td>🏆</td>}
                 {(star.wonEmmy === true) && <td>🏆</td>}
               </tr>
             )}
-          </tr>
         </tbody>
       </table>
+      <button onClick={randomStar}>Add Random</button>
     </div>
   );
 }
