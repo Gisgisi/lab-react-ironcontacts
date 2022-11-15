6aa93392fb29dca144020ee86c53cfd0c078b43c
John Doe <atarod.shirin@gmail.com>
2022-11-15 16:48:07 +0100

    it2 done

--- src/App.js
+++ src/App.js
@@ -9,12 +9,15 @@ function App() {
   console.log(contacts)
   return (
     <div className="App">
+      <h1>IronContacts</h1>
       <table>
         <thead>
-          <tr>
+          <tr class='tablehead'>
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
           </tr>
         </thead>
         <tbody>
@@ -23,7 +26,9 @@ function App() {
               <tr key={star.id}>
                 <td><img class='starimg' src={star.pictureUrl}/></td>
                 <td>{star.name}</td>
-                <td>{star.popularity}</td>
+                <td>{(Math.round((star.popularity)*100))/100}</td>
+                {(star.wonOscar === true) && <td>🏆</td>}
+                {(star.wonEmmy === true) && <td>🏆</td>}
               </tr>
             )}
           </tr>
